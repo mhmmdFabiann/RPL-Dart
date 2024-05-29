8912ff7dfb1401d2ec1f37f2a637a05830593da0
aksaahrmwn <aksahermawan2913@students.unnes.ac.id>
2024-05-29 19:47:22 +0700

    mengubah login

--- RPL-flutterflow/lib/login/login_model.dart
+++ RPL-flutterflow/lib/login/login_model.dart
@@ -3,65 +3,85 @@ import 'login_widget.dart' show LoginWidget;
 import 'package:flutter/material.dart';
 
 class LoginModel extends FlutterFlowModel<LoginWidget> {
-  ///  State fields for stateful widgets in this page.
+  /// State fields for stateful widgets in this page.
 
   final unfocusNode = FocusNode();
-  // State field(s) for TabBar widget.
-  TabController? tabBarController;
-  int get tabBarCurrentIndex =>
-      tabBarController != null ? tabBarController!.index : 0;
 
-  // State field(s) for emailAddress widget.
-  FocusNode? emailAddressFocusNode;
-  TextEditingController? emailAddressTextController;
+  // State fields for TabBar widget.
+  TabController? tabBarController;
+  int get tabBarCurrentIndex => tabBarController?.index ?? 0;
+
+  // State fields for email and password widgets.
+  late FocusNode emailAddressFocusNode;
+  late TextEditingController emailAddressTextController;
   String? Function(BuildContext, String?)? emailAddressTextControllerValidator;
-  // State field(s) for password widget.
-  FocusNode? passwordFocusNode;
-  TextEditingController? passwordTextController;
+  
+  late FocusNode passwordFocusNode;
+  late TextEditingController passwordTextController;
   late bool passwordVisibility;
   String? Function(BuildContext, String?)? passwordTextControllerValidator;
-  // State field(s) for emailAddress_Create widget.
-  FocusNode? emailAddressCreateFocusNode;
-  TextEditingController? emailAddressCreateTextController;
-  String? Function(BuildContext, String?)?
-      emailAddressCreateTextControllerValidator;
-  // State field(s) for password_Create widget.
-  FocusNode? passwordCreateFocusNode;
-  TextEditingController? passwordCreateTextController;
+  
+  late FocusNode emailAddressCreateFocusNode;
+  late TextEditingController emailAddressCreateTextController;
+  String? Function(BuildContext, String?)? emailAddressCreateTextControllerValidator;
+  
+  late FocusNode passwordCreateFocusNode;
+  late TextEditingController passwordCreateTextController;
   late bool passwordCreateVisibility;
-  String? Function(BuildContext, String?)?
-      passwordCreateTextControllerValidator;
-  // State field(s) for passwordConfirm widget.
-  FocusNode? passwordConfirmFocusNode;
-  TextEditingController? passwordConfirmTextController;
+  String? Function(BuildContext, String?)? passwordCreateTextControllerValidator;
+  
+  late FocusNode passwordConfirmFocusNode;
+  late TextEditingController passwordConfirmTextController;
   late bool passwordConfirmVisibility;
-  String? Function(BuildContext, String?)?
-      passwordConfirmTextControllerValidator;
+  String? Function(BuildContext, String?)? passwordConfirmTextControllerValidator;
 
   @override
   void initState(BuildContext context) {
+    super.initState();
+
+    // Initialize visibility
     passwordVisibility = false;
     passwordCreateVisibility = false;
     passwordConfirmVisibility = false;
+
+    // Initialize FocusNodes and TextControllers
+    emailAddressFocusNode = FocusNode();
+    emailAddressTextController = TextEditingController();
+
+    passwordFocusNode = FocusNode();
+    passwordTextController = TextEditingController();
+
+    emailAddressCreateFocusNode = FocusNode();
+    emailAddressCreateTextController = TextEditingController();
+
+    passwordCreateFocusNode = FocusNode();
+    passwordCreateTextController = TextEditingController();
+
+    passwordConfirmFocusNode = FocusNode();
+    passwordConfirmTextController = TextEditingController();
   }
 
   @override
   void dispose() {
+    // Dispose FocusNodes and TextControllers
     unfocusNode.dispose();
     tabBarController?.dispose();
-    emailAddressFocusNode?.dispose();
-    emailAddressTextController?.dispose();
 
-    passwordFocusNode?.dispose();
-    passwordTextController?.dispose();
+    emailAddressFocusNode.dispose();
+    emailAddressTextController.dispose();
 
-    emailAddressCreateFocusNode?.dispose();
-    emailAddressCreateTextController?.dispose();
+    passwordFocusNode.dispose();
+    passwordTextController.dispose();
 
-    passwordCreateFocusNode?.dispose();
-    passwordCreateTextController?.dispose();
+    emailAddressCreateFocusNode.dispose();
+    emailAddressCreateTextController.dispose();
 
-    passwordConfirmFocusNode?.dispose();
-    passwordConfirmTextController?.dispose();
+    passwordCreateFocusNode.dispose();
+    passwordCreateTextController.dispose();
+
+    passwordConfirmFocusNode.dispose();
+    passwordConfirmTextController.dispose();
+
+    super.dispose();
   }
 }
