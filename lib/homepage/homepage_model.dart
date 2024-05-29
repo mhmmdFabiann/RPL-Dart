55de654d22d08921500dda76fc8f8047ebd3fddb
aishaxx14 <aishaarsanti1408@gmail.com>
2024-05-29 19:38:30 +0700

    mengubah homepage

--- lib/homepage/homepage_model.dart
+++ lib/homepage/homepage_model.dart
@@ -1,17 +1,23 @@
+import 'package:flutter/material.dart';
 import '/flutter_flow/flutter_flow_util.dart';
 import 'homepage_widget.dart' show HomepageWidget;
-import 'package:flutter/material.dart';
 
 class HomepageModel extends FlutterFlowModel<HomepageWidget> {
-  ///  State fields for stateful widgets in this page.
+  /// State fields for stateful widgets in this page.
+  final FocusNode unfocusNode;
 
-  final unfocusNode = FocusNode();
+  /// Constructor
+  HomepageModel() : unfocusNode = FocusNode();
 
   @override
-  void initState(BuildContext context) {}
+  void initState(BuildContext context) {
+    // Add initialization code here if needed
+  }
 
   @override
   void dispose() {
+    // Dispose the focus node to free up resources
     unfocusNode.dispose();
+    super.dispose();
   }
 }
